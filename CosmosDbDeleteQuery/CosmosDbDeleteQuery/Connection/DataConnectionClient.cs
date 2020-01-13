a58ce096878d6100ccc7b6d80a52ca28c15d6aa5
Jordi Jolink <info@soneritics.nl>
2020-01-13 13:40:33 +0100

    Add Program Flow

---
+++ CosmosDbDeleteQuery/CosmosDbDeleteQuery/Connection/DataConnectionClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace CosmosDbDeleteQuery.Connection
+{
+    /// <summary>
+    /// Data client abstraction.
+    /// </summary>
+    public class DataConnectionClient
+    {
+        public DocumentClient Client { get; }
+        public string DatabaseId { get; }
+        public string CollectionId { get; }
+        public bool EnableCrossPartitionQuery { get; }
+        public PartitionKey PartitionKey { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataConnectionClient"/> class.
+        /// </summary>
+        /// <param name="endpoint">The endpoint.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="databaseId">The database identifier.</param>
+        /// <param name="collectionId">The collection identifier.</param>
+        /// <param name="enableCrossPartitionQuery">if set to <c>true</c> [enable cross partition query].</param>
+        /// <param name="partitionKey">The partition key.</param>
+        public DataConnectionClient(string endpoint, string key, string databaseId, string collectionId, bool enableCrossPartitionQuery, string partitionKey = "id")
+        {
+            DatabaseId = databaseId;
+            CollectionId = collectionId;
+            EnableCrossPartitionQuery = enableCrossPartitionQuery;
+            PartitionKey = new PartitionKey(partitionKey);
+            Client = new DocumentClient(
+                serviceEndpoint: new Uri(endpoint),
+                authKeyOrResourceToken: key,
+                connectionPolicy: GetConnectionPolicy()
+            );
+        }
+
+        /// <summary>
+        /// Gets the connection policy.
+        /// For this application, only reads should be allowed which need the data directly.
+        /// Therefore, the policy is very strict. If the connection is unavailable, we don't
+        /// want the user to wait endlessly, but rather show an error that the database is down.
+        /// </summary>
+        /// <returns></returns>
+        protected ConnectionPolicy GetConnectionPolicy()
+        {
+            return new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                RequestTimeout = new TimeSpan(0, 0, 5),
+                RetryOptions = new RetryOptions
+                {
+                    MaxRetryAttemptsOnThrottledRequests = 3,
+                    MaxRetryWaitTimeInSeconds = 1
+                }
+            };
+        }
+    }
+}
---
+++ CosmosDbDeleteQuery/CosmosDbDeleteQuery/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using CosmosDbDeleteQuery.Connection;
+
+namespace CosmosDbDeleteQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this Cosmos Db tool, that helps you execute delete queries.");
+            Console.WriteLine("WARNING: This tool will actually delete, not soft-delete.");
+            Console.WriteLine("Use the Microsoft Azure Storage Explorer to write your query, then use this tool to delete multiple documents.\r\n");
+            if (!GetBoolVal("Are you sure you want to proceed?", true))
+                return;
+
+            var dataConnectionClient = GetDataConnectionClient();
+            var proceed = true;
+            while (proceed)
+            {
+                var query = GetWhereClause();
+
+                var count = 10;
+                Console.WriteLine($"Your query has {count} result(s).");
+
+                if (count > 0 && GetBoolVal("Are you completely sure you want to delete these documents", false))
+                    Console.WriteLine("Deleted");
+
+                proceed = GetBoolVal("Execute another query", false);
+            }
+        }
+
+        private static DataConnectionClient GetDataConnectionClient()
+        {
+            Console.Clear();
+            Console.WriteLine("Please first provide us with the connection details.");
+            var endpoint = GetStringVal("Endpoint", "https://localhost:8081");
+            var key = GetStringVal("Account key", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+            var databaseId = GetStringVal("Database", string.Empty);
+            var collectionId = GetStringVal("Collection", string.Empty);
+            var enableCrossPartitionQuery = GetBoolVal("Enable cross partition querying", false);
+            var partitionKey = enableCrossPartitionQuery ? GetStringVal("Cross partition query partition key", "id") : string.Empty;
+
+            return new DataConnectionClient(
+                endpoint,
+                key,
+                databaseId,
+                collectionId,
+                enableCrossPartitionQuery,
+                partitionKey
+            );
+        }
+
+        private static string GetWhereClause()
+        {
+            Console.Clear();
+            Console.WriteLine("Please write your WHERE clause below. When you enter an empty line, the query will be executed.\r\n\r\n");
+            Console.WriteLine("DELETE FROM c WHERE");
+
+            var query = new StringBuilder();
+            string input;
+            do
+            {
+                query.Append((input = Console.ReadLine()) + " ");
+            } while (!string.IsNullOrEmpty(input));
+
+            var result = query.ToString();
+            Console.Clear();
+            Console.WriteLine("You entered the following query:\r\n\r\n");
+            Console.WriteLine($"DELETE FROM c WHERE {result}");
+
+            if (!GetBoolVal("\r\n\r\nAre you sure you want to execute this query?", true))
+                return GetWhereClause();
+
+            return result;
+        }
+
+        private static string GetStringVal(string label, string defaultValue = "")
+        {
+            Console.Write($"{label}: ({defaultValue}) ");
+            var val = Console.ReadLine();
+
+            return string.IsNullOrEmpty(val) ? defaultValue : val;
+        }
+
+        private static bool GetBoolVal(string label, bool defaultValue)
+        {
+            Console.Write($"{label}: y/n (" + (defaultValue ? "y" : "n") + ") ");
+            var val = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(val))
+                return defaultValue;
+
+            var v = val.ToLower();
+
+            if (v == "y")
+                return true;
+            else if (v == "n")
+                return false;
+
+            Console.WriteLine("Invalid value. Typ y or n. Try again.");
+            return GetBoolVal(label, defaultValue);
+        }
+    }
+}
